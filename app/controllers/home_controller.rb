ffe550ee221a75875e36499e8518acc5583e4e26
zenjutahi <zenjutahi@gmail.com>
2020-01-21 19:42:54 +0300

    [feature] data response and display

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @stockApi = StockQuote::Stock.new(api_key: "pk_36376d6cf9f64da9a2fa81f7a843ecea")
+    @stock_api = StockQuote::Stock.new(api_key: "pk_36376d6cf9f64da9a2fa81f7a843ecea")
+    if params[:ticker]
+      @stock = StockQuote::Stock.quote(params[:ticker])
+      @comapany_data = StockQuote::Stock.company(params[:ticker])
+      @logo = StockQuote::Stock.logo(params[:ticker])
+    end
   end
 
   def about
