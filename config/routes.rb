551aca8f7b256511235677a5e3e0ca25499d7bd7
zenjutahi <jeffmnjogu@gmail.com>
2020-01-21 12:18:50 +0300

    [chore] pre-setup /contollers/pages/

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # create the root/home route
+  root 'home#index'
+
 end
