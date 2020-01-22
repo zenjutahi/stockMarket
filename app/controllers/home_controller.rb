ed5027598018aa9aaa7932e1f90defe15944abb3
zenjutahi <zenjutahi@gmail.com>
2020-01-22 11:46:03 +0300

    [chore] nothing error handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,9 @@
 class HomeController < ApplicationController
   def index
     @stock_api = StockQuote::Stock.new(api_key: "pk_36376d6cf9f64da9a2fa81f7a843ecea")
-    if params[:ticker]
+    if params[:ticker] == ""
+      @nothing = "Hey! You forgot to Enter A Symbol"
+    elsif params[:ticker]
       @stock = StockQuote::Stock.quote(params[:ticker])
       @comapany_data = StockQuote::Stock.company(params[:ticker])
       @logo = StockQuote::Stock.logo(params[:ticker])
