24659161350496c073fa02ad14e909f6a36af449
zenjutahi <zenjutahi@gmail.com>
2020-02-05 11:57:42 +0300

    [chore] generate stocks table via scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_22_094709) do
+ActiveRecord::Schema.define(version: 2020_02_05_085338) do
+
+  create_table "stocks", force: :cascade do |t|
+    t.string "ticker"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_stocks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
